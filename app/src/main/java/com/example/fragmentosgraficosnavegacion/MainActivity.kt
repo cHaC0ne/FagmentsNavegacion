0131d61b80020b49b3936530fdb5f5b4505eb015
Alumno <asd@asd.com>
2022-07-05 17:28:23 +0200

    COn menu

--- app/src/main/java/com/example/fragmentosgraficosnavegacion/MainActivity.kt
+++ app/src/main/java/com/example/fragmentosgraficosnavegacion/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.fragmentosgraficosnavegacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.fragmentosgraficosnavegacion.databinding.ActivityMainBinding
 
 
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+
+        b.bNavView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.bInicio -> {
+                    Navigation.findNavController(b.fragmentContainerView)
+                        .navigate(R.id.initFrag)
+                    true
+                }
+                R.id.bProfile -> {
+                    findNavController(R.id.fragmentContainerView)
+                        .navigate(R.id.fristFrag)
+                    true
+                }
+                R.id.bSetting -> {
+                    b.fragmentContainerView.findNavController()
+                        .navigate(R.id.secondFrag)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
